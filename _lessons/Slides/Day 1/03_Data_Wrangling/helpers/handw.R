44408b1b5cc99252882b65af17419c2394ef8948
Markus <freitagmarkus@outlook.de>
2021-07-13 15:04:26 +0200

    updates reshedule

--- _lessons/Slides/Day 1/03_Data_Wrangling/helpers/handw.R
+++
@@ -1,8 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/vara@1.2.0/src/vara.min.js" type="text/javascript"></script>
-  
-  
-  new Vara("#element","font.json",[{
-    text:"Handwritten"
-  }],{
-    fontSize:46
-  }); 
---
+++ _lessons/Slides/Day 2/06_Functions/helpers/handw.R
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/vara@1.2.0/src/vara.min.js" type="text/javascript"></script>
+  
+  
+  new Vara("#element","font.json",[{
+    text:"Handwritten"
+  }],{
+    fontSize:46
+  }); 
